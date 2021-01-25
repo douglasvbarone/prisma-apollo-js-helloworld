4e08a006c5801fdcd2ad5df2fc224affcbcf09b4
Douglas Barone <douglasvbarone@gmail.com>
2021-01-25 11:38:42 -0400

    Catch server errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const { server } = require('./server')
 
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+server
+  .listen()
+  .then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`)
+  })
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  
