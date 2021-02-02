0e39913c80dba8ad5d89cf5d1481fd9c2cdb86d1
Douglas Barone <douglasvbarone@gmail.com>
2021-02-02 18:00:31 -0400

    Make it orange... Why not?!

--- web/src/views/Drafts.vue
+++ web/src/views/Drafts.vue
@@ -3,8 +3,14 @@
     <v-row>
       <v-col v-for="post in posts" :key="post.id" cols="12" md="6">
         <v-hover v-slot="{ hover }">
-          <router-link :to="`/post/${post.id}/edit`" class="text-decoration-none">
-            <v-card class="fill-height pop" :elevation="hover ? 24 : 5">
+          <router-link
+            :to="`/post/${post.id}/edit`"
+            class="text-decoration-none"
+          >
+            <v-card
+              class="fill-height pop orange lighten-4"
+              :elevation="hover ? 24 : 5"
+            >
               <v-card-title class="headline font-weight-light">
                 <v-icon left>mdi-pencil</v-icon> {{ post.title }}
               </v-card-title>
@@ -26,9 +32,7 @@
                   </v-row>
                 </v-container>
               </v-card-text>
-              <v-card-text>
-                {{ post.intro }}...
-              </v-card-text>              
+              <v-card-text> {{ post.intro }}... </v-card-text>
             </v-card>
           </router-link>
         </v-hover>
@@ -53,7 +57,7 @@ export default {
             intro
             published
             createdAt
-            
+
             author {
               name
               profile {
